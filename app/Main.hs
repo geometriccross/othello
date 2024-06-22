51daa27d548d8228dded322343b6a42ca72495e1
geometriccross <geometrccross@gmail.com>
2024-06-22 16:45:47 +0900

    setup template

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
-module Main (main) where
-
+import Graphics.Gloss 
 import Lib
 
 main :: IO ()
-main = someFunc
+main = display inWindow white message
+  where
+    inWindow = InWindow "Haskell Day 2018" (640, 480) (100, 100)
+    message = text "Hello World"
